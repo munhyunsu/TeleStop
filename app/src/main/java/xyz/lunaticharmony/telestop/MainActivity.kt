d2644af52432f1a01bd07e97d8b32add2ad94c30
munhyunsu <munhyunsu@gmail.com>
2021-11-26 18:40:47 +0900

    Button action

--- app/src/main/java/xyz/lunaticharmony/telestop/MainActivity.kt
+++ app/src/main/java/xyz/lunaticharmony/telestop/MainActivity.kt
@@ -2,10 +2,16 @@ package xyz.lunaticharmony.telestop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun checkButton(view: View) {
+        Toast.makeText(view.context, R.string.msg_float, Toast.LENGTH_SHORT).show();
     }
 }

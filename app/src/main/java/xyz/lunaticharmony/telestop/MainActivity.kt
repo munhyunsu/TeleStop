de1402bf0c5e07274866a8d1e63d6b02a2f172a8
munhyunsu <munhyunsu@gmail.com>
2022-05-16 08:44:15 +0900

    Update kotlin

--- app/src/main/java/xyz/lunaticharmony/telestop/MainActivity.kt
+++ app/src/main/java/xyz/lunaticharmony/telestop/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun checkButton(view: View) {
+//        R.id.extendedFloatingActionButton.extend()
+//        val efab: ExtendedFloatingActionButton = findViewById(R.id.extendedFloatingActionButton)
+//        efab.extend()
         Toast.makeText(view.context, R.string.msg_float, Toast.LENGTH_SHORT).show();
     }
 }
